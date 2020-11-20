aaac03e99ed8304b4e6171442a00c992f574fe68
zx1106 <2251067853@qq.com>
2020-11-20 15:29:40 +0800

    网易严选初始化

---
+++ js/goods.js
@@ -0,0 +1,10 @@
+class Cat {
+  constructor(n){
+    this.name = n
+  }
+  skill(){
+    console.log('miao')
+  }
+}
+let cat1 = new Cat('嘿嘿')
+cat1.skill()
---
+++ js/list.js
@@ -0,0 +1,5 @@
+function test(){
+  var name = 'xm';
+  var age = 23;
+  console.log(name);
+};
